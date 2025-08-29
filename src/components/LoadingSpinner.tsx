ed1a6ccbfd136b8b0b80ed42ed189684e5c7f705
kshitijpoudyal <kshitijpoudyal@gmail.com>
2025-08-29 16:10:35 -0400

    move login to different page and create 404 error pageclear

--- src/components/LoadingSpinner.tsx
+++ src/components/LoadingSpinner.tsx
@@ -4,7 +4,7 @@ const LoadingSpinner: React.FC = () => {
   return (
     <div className="text-center py-10 text-gray-500">
       <div className="spinner mx-auto mb-4"></div>
-      <p>Loading data...</p>
+      {/* <p>Loading data...</p> */}
     </div>
   );
 };
